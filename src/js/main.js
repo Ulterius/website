be66b5bef0eca74ec40bbc128bdc3f153acaf1d9
Jonathan Reed <jontonsoup4@gmail.com>
2016-09-09 23:52:16 -0400

    clean js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
-var mobileMenuToggle, mobileMenu;
-
 /**
  * Mobile menu handling
  */
-mobileMenuToggle = document.getElementById('menu-toggle');
-mobileMenu = document.getElementById('mobile-nav');
-
-console.log(mobileMenu);
-console.log(mobileMenuToggle);
+var mobileMenuToggle = document.getElementById('menu-toggle');
+var mobileMenu = document.getElementById('mobile-nav');
+var hamburger = document.getElementById('hamburger');
 
 mobileMenuToggle.onclick =  function (e) {
-    document.getElementById('hamburger').classList.toggle('is-active');
+    hamburger.classList.toggle('is-active');
     mobileMenu.classList.toggle('mobile-nav--visible');
     document.body.classList.toggle('menu-visible');
     e.preventDefault();
